36dabd932e501282bb27c3869a812fd1fa4e5e50
kajal-2011 <kajalrekha888@gmail.com>
2023-03-07 10:20:23 +0600

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,37 +3,53 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const validator = require("validator");
 
+// generate token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
 // register user
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const exist = await userModel.findOne({ email });
+    const exist = await userModel.findOne({ email });
 
-  if (exist) {
-    return res.status(400).json("Email already exist");
+    if (exist) {
+      return res.status(400).json("Email already exist");
+    }
+
+    // validation
+    if (!name || !email || !password) {
+      return res.status(400).json("All fields are required");
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("Invalied email");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res
+        .status(400)
+        .json(
+          "Password does not meet complexity requirements. Please choose a stronger password."
+        );
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // create an user
+    const user = await userModel.create({ name, email, password: hash });
+
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ ...user, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-
-  // validation
-  if (!name || !email || !password) {
-    return res.status(400).json("All fields are required");
-  }
-
-  if (!validator.isEmail(email)) {
-    return res.status(400).json("Invalied email");
-  }
-
-  if (!validator.isStrongPassword(password)) {
-    return res
-      .status(400)
-      .json(
-        "Password does not meet complexity requirements. Please choose a stronger password."
-      );
-  }
-
-  // hash password
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
 };
 
 module.exports = {
